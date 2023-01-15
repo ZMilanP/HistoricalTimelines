7f4fdd6997132291d82ec57d11368f1054fa8a9a
ZMilanP <vandalizam@msn.com>
2023-01-15 09:56:48 +0100

    Add project files.

---
+++ EFModel.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalTimelines
+{
+    public class HistoricalEntities : DbContext
+    {
+        public DbSet<Entity> Entities { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public string DbPath { get; }
+
+        public HistoricalEntities()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "deafult.db");
+        }
+
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer($"Data Source={DbPath}");
+    }
+
+    public class Entity
+    {
+        public int EntityId { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+        public DateTime Date { get; set; }
+
+        public List<Person> Persons { get; } = new();
+        public List<Building> Buildings { get; } = new();
+    }
+
+    public class Person
+    {
+        public int NameId { get; set; }
+        public string Name { get; set; }
+        public string? Title { get; set; }
+        public string? Lastname { get; set; }
+
+        public DateTime Time_Birth { get; set; }
+        public DateTime? Time_Death { get; set; }    
+
+        public Entity Entity { get; set; }
+
+        
+    }
+    public class Building
+    {
+        public int NameId { get; set; }
+        public string Name { get; set; }
+        public DateTime Built { get; set; }
+        public DateTime? Razed { get; set; }
+        public Entity Entity { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using HistoricalTimelines;
+using System;
+using System.Linq;
+
+using var db = new HistoricalEntities();
+
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new Entity");
+db.Add(new Person { Name  = "Henry IV", Title = "King of England" });
+db.Add(new Building { Name = "Versailles", Built = new DateTime (1661) });
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for an Entity");
+var entity = db.Entities
+    .OrderBy(b => b.EntityId)
+    .First();
+
+// Update
+Console.WriteLine("Updating the database and adding an Entity");
+entity.Time_Birth = new DateTime (1367, (DateTimeKind)6);
+entity.Persons.Add(
+    new Person { Name = "Richard I Lionheart", Title = "King of England" });
+db.SaveChanges();
+
+// Delete
+Console.WriteLine("Delete the Entity");
+db.Remove(entity);
+db.SaveChanges();
