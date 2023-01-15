c54306ee4596b9b2249d19a8f7bf4585c55e4ff9
ZMilanP <vandalizam@msn.com>
2023-01-15 09:56:48 +0100

    Some modifications, highly unsatisfactory, requires a more complex separation and clasification of entriesAdd project files.

---
+++ EFModel.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricalTimelines
+{
+    public class HistoricalEntities : DbContext
+    {
+        public DbSet<Entity> Entities { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public string DbPath { get; }
+
+        public HistoricalEntities()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "deafult.db");
+        }
+
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer($"Data Source={DbPath}");
+    }
+    
+    public class Entity
+    {
+        public int EntityId { get; set; }
+        public decimal Coordinates { get; set; }
+
+        public List<Person> Persons { get; } = new();
+        public List<Building> Buildings { get; } = new();
+    }
+
+    public class Person
+    {
+        public int NameId { get; set; }
+        public string Name { get; set; }
+        public string? Lastname { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+
+
+        public DateOnly Time_Birth { get; set; }
+        public DateOnly? Time_Death { get; set; }    
+
+        public Entity Entity { get; set; }
+
+        
+    }
+    public class Building
+    {
+        public int NameId { get; set; }
+        public string Name { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public DateOnly Built { get; set; }
+        public DateOnly? Razed { get; set; }
+        public Entity Entity { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+using HistoricalTimelines;
+using System;
+using System.Linq;
+
+using var db = new HistoricalEntities();
+
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new Entity");
+db.Add(new Person {Name = "Henry IV, King of England", 
+                   Latitude = 53.164140M, 
+                   Longitude = 0.0163800M, 
+                   Time_Birth = new DateOnly ( 1367, 4, 1 ), 
+                   Time_Death = new DateOnly ( 1413, 3, 20) });
+db.Add(new Building {Name = "Versailles", 
+                     Latitude = 48.801407M,
+                     Longitude = 2.130122M,
+                     Built = new DateOnly (1661, 07, 17) });
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for an Entity");
+var entity = db.Entities
+    .OrderBy(b => b.EntityId)
+    .First();
+
+// Update
+Console.WriteLine("Updating the database and adding an Entity");
+db.Entities.First(a => a.EntityId == 2);
+entity.Persons.Add(
+    new Person {Name = "Richard I Lionheart, King of England",
+                Time_Birth = new DateOnly (1157, 9, 8),
+                Latitude = 51.7550582885742M,
+                Longitude = -1.26209998130798M,
+                Time_Death = new DateOnly (1199, 4, 6)
+    });
+db.SaveChanges();
+
+// Delete
+Console.WriteLine("Delete the Entity");
+db.Remove(entity);
+db.SaveChanges();
